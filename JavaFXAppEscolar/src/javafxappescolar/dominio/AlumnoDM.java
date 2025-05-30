aca3e0b32e8a3d30ce4e3cc67b1f16cdf30320b7
Miguel <miguelmendez765@gmail.com>
2025-05-30 08:55:31 -0600

    Aplicacion app escolar

---
+++ JavaFXAppEscolar/src/javafxappescolar/dominio/AlumnoDM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxappescolar.dominio;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafxappescolar.modelo.dao.AlumnoDAO;
+import javafxappescolar.modelo.pojo.ResultadoOperacion;
+
+/**
+ *
+ * @author migue
+ */
+public class AlumnoDM {
+ 
+    
+}
